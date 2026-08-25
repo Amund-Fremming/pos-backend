d0c80a8fb360746dbc69f0bc71d86480890d4784
Amund Fremming <amund.fremming@gmail.com>
2026-08-25 21:56:45 +0200

    ssss

--- src/db/setup.rs
+++ src/db/setup.rs
@@ -3,8 +3,12 @@ use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 pub const MAX_POOL_CONNECTIONS: u32 = 20;
 
 pub async fn create_pool(database_url: &str) -> Result<Pool<Postgres>, sqlx::Error> {
-    PgPoolOptions::new()
+    let pool = PgPoolOptions::new()
         .max_connections(MAX_POOL_CONNECTIONS)
         .connect(database_url)
-        .await
+        .await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(pool)
 }
